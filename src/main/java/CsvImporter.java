9aa0c261b9ad75fc80ff7439be74e68687a18065
tama3ti <m.picheta@icloud.com>
2022-02-24 21:36:16 +0100

    changed final sudoku in main to csv imported sudoku

--- src/main/java/CsvImporter.java
+++ src/main/java/CsvImporter.java
@@ -3,17 +3,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CsvImporter {
-    private CsvImporter() {}
+    public CsvImporter() {
 
+    }
 
-    public static int[][] run() throws FileNotFoundException {
+    public int[][] run() throws FileNotFoundException {
         File file = null;
         Scanner myObj = new Scanner(System.in);
 
         try {
             System.out.println("Input file path >");
             file = new File(myObj.nextLine()); //Input dir
-            System.out.println("Loading file... " + file);  // Output user input
+            System.out.println("Loading file... ");  // Output user input
 
         } catch (IllegalArgumentException iae) {
             System.out.println("File Not Found");
@@ -26,7 +27,7 @@ public class CsvImporter {
     /*
     Takes out the data from the file submitted by the user
     */
-    public static String getFileString(File path) throws FileNotFoundException {
+    public String getFileString(File path) throws FileNotFoundException {
         //No need for a try-cache since it checks for it above
         Scanner myReader = new Scanner(path);
         String data = myReader.nextLine();
@@ -36,7 +37,7 @@ public class CsvImporter {
     /*
     Goes through the String and adds every integer into an arraylist
     */
-    public static ArrayList<Integer> getArrayFromString(String data){
+    public ArrayList<Integer> getArrayFromString(String data){
         ArrayList<Integer> testArray = new ArrayList<>();
 
         for (int i = 0; i < data.length(); i++) {
@@ -50,7 +51,7 @@ public class CsvImporter {
     /*
     Makes an matrix out of a number arraylist using math
      */
-    public static int[][] getMatrixFromArray(ArrayList<Integer> data){
+    public int[][] getMatrixFromArray(ArrayList<Integer> data){
         int[][] matrix = new int[9][9];
 
         for (int i = 0; i < 9; i++) {
--- src/main/java/SudokuValidator.java
+++ src/main/java/SudokuValidator.java
@@ -3,18 +3,19 @@ import java.io.FileNotFoundException;
 public class SudokuValidator {
 
     public static int[][] sudoku = {
-            {6, 2, 4, 5, 3, 9, 1, 8, 7},
+            {1, 2, 4, 5, 3, 9, 1, 8, 7},
             {5, 1, 9, 7, 2, 8, 6, 3, 4},
             {8, 3, 7, 6, 1, 4, 2, 9, 5},
             {1, 4, 3, 8, 6, 5, 7, 2, 9},
             {9, 5, 8, 2, 4, 7, 3, 6, 1},
-            {7, 6, 2, 3, 1, 1, 4, 5, 8},
+            {7, 6, 2, 3, 9, 1, 4, 5, 8},
             {3, 7, 1, 9, 5, 6, 8, 4, 2},
             {4, 9, 6, 1, 8, 2, 5, 7, 3},
             {2, 8, 5, 4, 7, 3, 9, 1, 6}
     };
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+        sudoku = new CsvImporter().run();
 
         RowValidator rowValidator = new RowValidator(0, 0);
         ColumnValidator columnValidator = new ColumnValidator(0, 0);
